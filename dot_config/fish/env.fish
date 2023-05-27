493816c7f31a003d27e5f6b83b3e50c3cd6b6492
Connor Rhodes <connor@rhodes.contact>
2023-05-27 18:16:27 -0500

    added macOS definition of LIMA_HOME

--- dot_config/fish/env.fish
+++ dot_config/fish/env.fish
@@ -3,3 +3,8 @@ set --export PATH $HOME/.local/bin $PATH
 
 # program configs
 set --universal FZF_DEFAULT_COMMAND 'fd'
+
+# macOS only
+{{if eq .chezmoi.os "darwin"}}
+set --export LIMA_HOME "$HOME/.config/lima"
+{{end}}
