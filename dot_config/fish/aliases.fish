efa7dcfac8017da24e28bec84b7031f58b68cc60
Connor Rhodes <connor@rhodes.contact>
2023-05-27 14:40:54 -0500

    added git aliases and cc alias

--- dot_config/fish/aliases.fish
+++ dot_config/fish/aliases.fish
@@ -1,3 +1,11 @@
 alias e="nvim"
 alias j="__zoxide_z"
 alias cm="chezmoi"
+alias cc="cd (fd -t d |fzf)" # cd down the directory structure with fzf
+
+# git aliases
+alias gs="git status"
+
+function gcam --wraps git --description 'git commit -a -m'
+    git commit -a -m "$argv"
+end
